92d70f295b18855834827ca0e6fea239fb5a75cf
nikita <nikitaluftiev@gmail.com>
2022-11-18 18:40:36 +0300

    Change the indentation size

--- src/Footer/Footer.tsx
+++ src/Footer/Footer.tsx
@@ -2,39 +2,39 @@ import { Box, Grid, Link, Typography } from "@mui/material";
 import { makeStyles } from "@mui/styles";
 
 const useStyles = makeStyles(() => ({
-    root: {
-        padding: '30px',
-        '& .MuiLink-root': {
-            whiteSpace: 'nowrap',
-            color: 'white !important'
-        },
-        textAlign: 'center'
-    }
+  root: {
+    padding: "30px",
+    "& .MuiLink-root": {
+      whiteSpace: "nowrap",
+      color: "white !important",
+    },
+    textAlign: "center",
+  },
 }));
 
 const Footer = () => {
-    const classes = useStyles();
-    
-    return (
-        <Box className={classes.root}>
-            <Grid container spacing={2} sx={{mb: '30px'}}>
-                <Grid item xs={6}>
-                    <Link>Reward Policy</Link>
-                </Grid>
-                <Grid item xs={6}>
-                    <Link>Terms of Use</Link>
-                </Grid>
-                <Grid item xs={6}>
-                    <Link>Privacy Policy</Link>
-                </Grid>
-                <Grid item xs={6}>
-                    <Link>Cookie Policy</Link>
-                </Grid>
-            </Grid>
-            <Typography color='white'>@TreatCoin</Typography>
-            <Typography color='#7674F5'>Allrights Reserved 2022</Typography>
-        </Box>
-    )
-}
+  const classes = useStyles();
+
+  return (
+    <Box className={classes.root}>
+      <Grid container spacing={2} sx={{ mb: "30px" }}>
+        <Grid item xs={6}>
+          <Link>Reward Policy</Link>
+        </Grid>
+        <Grid item xs={6}>
+          <Link>Terms of Use</Link>
+        </Grid>
+        <Grid item xs={6}>
+          <Link>Privacy Policy</Link>
+        </Grid>
+        <Grid item xs={6}>
+          <Link>Cookie Policy</Link>
+        </Grid>
+      </Grid>
+      <Typography color="white">@TreatCoin</Typography>
+      <Typography color="#7674F5">Allrights Reserved 2022</Typography>
+    </Box>
+  );
+};
 
 export default Footer;
--- src/components/SwiperContainer.tsx
+++ src/components/SwiperContainer.tsx
@@ -9,59 +9,59 @@ import { Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 
 const SwiperContainer = () => {
-    const [swiperData, setSwiperData] = useState<number[]>([]);
+  const [swiperData, setSwiperData] = useState<number[]>([]);
 
-    const getData = async () => {
-        const data = await fetch("Data/Data.json", {
-            headers: {
-                "Content-Type": "application/json",
-                Accept: "application/json",
-            },
-        }).then(function (response) {
-            return response.json();
-        });
+  const getData = async () => {
+    const data = await fetch("Data/Data.json", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    }).then(function (response) {
+      return response.json();
+    });
 
-        setSwiperData(data.treatCoinSwiperData);
-    };
+    setSwiperData(data.treatCoinSwiperData);
+  };
 
-    useEffect(() => {
-        getData();
-    }, []);
+  useEffect(() => {
+    getData();
+  }, []);
 
-    return (
-        <>
-            <Swiper
-                slidesPerView={2}
-                spaceBetween={50}
-                freeMode={true}
-                centeredSlides={true}
-                loop={true}
-                pagination={{
-                    clickable: true,
+  return (
+    <>
+      <Swiper
+        slidesPerView={2}
+        spaceBetween={50}
+        freeMode={true}
+        centeredSlides={true}
+        loop={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[FreeMode, Pagination]}
+        className="mySwiper"
+      >
+        {swiperData.map((item, i) => {
+          return (
+            <SwiperSlide key={i}>
+              <Typography
+                variant="h5"
+                fontSize={41}
+                fontWeight={500}
+                sx={{
+                  userSelect: "none",
                 }}
-                modules={[FreeMode, Pagination]}
-                className="mySwiper"
-            >
-                {swiperData.map((item, i) => {
-                    return (
-                        <SwiperSlide key={i}>
-                            <Typography
-                                variant="h5"
-                                fontSize={41}
-                                fontWeight={500}
-                                sx={{
-                                    userSelect: "none",
-                                }}
-                            >
-                                {item.toLocaleString("en-US")}
-                                <sub style={{ fontSize: "14px" }}>TC</sub>
-                            </Typography>
-                        </SwiperSlide>
-                    );
-                })}
-            </Swiper>
-        </>
-    );
+              >
+                {item.toLocaleString("en-US")}
+                <sub style={{ fontSize: "14px" }}>TC</sub>
+              </Typography>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </>
+  );
 };
 
 export default SwiperContainer;
