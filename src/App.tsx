b4a2b5b0229424bb2db97cddde95719a2ccd3a1e
nikita <nikitaluftiev@gmail.com>
2022-11-22 12:18:01 +0300

    Change paths by using configuration file

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,21 @@ import "./App.css";
 import "./styles/global.css";
 import React from "react";
 
+const configuration = require('./configuration.json');
+const access_points = configuration.access_points;
+
+const path_home = access_points.home;
+const path_reset = access_points.reset;
+const path_reset_password = access_points.path_reset_password;
+
 class App extends React.Component {
     render(){
         return (
             <BrowserRouter>
                 <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/reset-password" element={<ResetPassword />} />
-                    <Route path="/reset" element={<Reset />} />
+                    <Route path={path_home} element={<Home />} />
+                    <Route path={path_reset} element={<ResetPassword />} />
+                    <Route path={path_reset_password} element={<Reset />} />
                 </Routes>
             </BrowserRouter>
         );
